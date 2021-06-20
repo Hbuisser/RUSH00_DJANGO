4ffad4bef3f148f2450acd9f3a16c3554110817b
Henry Buisseret <henrybuisseret@gmail.com>
2021-06-20 19:08:04 +0200

    ok

--- rush00/moviemon/imdb_skrap.py
+++ rush00/moviemon/imdb_skrap.py
@@ -5,15 +5,19 @@ import pickle
 import random
 
 class Data():
-    def __init__(self, movebals=None, p_names=None, info=None, pos_l=0, pos_c=0, out_of_range=False):
+    def __init__(self, bals_nbr=0, p_names=None, info=None, pos_l=0, pos_c=0, out_right=False, out_left=False, out_up=False, out_down=False, bals_pos=None, movie_pos=None):
         self.size = [10, 10]
-        self.movebals = movebals
+        self.bals_nbr = bals_nbr
         self.pos_l = pos_l
         self.pos_c = pos_c
-        self.out_of_range = out_of_range
+        self.out_right = out_right
+        self.out_left = out_left
+        self.out_up = out_up
+        self.out_down = out_down
         self.p_names = p_names
         self.info = info
-
+        self.bals_pos = bals_pos
+        self.movie_pos = movie_pos
 
 class Big_one():
     def __init__(self, d=None, my_list=None):
@@ -37,31 +41,39 @@ class Big_one():
         col_file = settings.GRID_SIZE[0]
         line_file = settings.GRID_SIZE[1]
         res = self.dump()
-        end = False
-        # try:
-            
-        # except:
-        #     print("Wrong file ")
+
         button = request.GET.get('button', None)
         if button == "right":
-            print(res.pos_c)
-            print(res.pos_l)
-            if (res.pos_c >= 0 and res.pos_c < col_file - 1):
+            if res.out_right == False:
                 res.pos_c = res.pos_c + 1
-        # else:
-        #     res.out_of_range = True
+                res.out_left = False
+            if res.pos_c == col_file - 1:
+                res.out_right = True
         elif button == "left":
-            print(res.pos_c)
-            print(res.pos_l)
-            if (res.pos_c >= 0 and res.pos_c < col_file - 1):
+            if res.out_left == False:
                 res.pos_c = res.pos_c - 1
+                res.out_right = False
+            if res.pos_c == 0:
+                res.out_left = True
         elif button == "up":
-            if (res.pos_l >= 0 and res.pos_l < line_file - 1):
+            if res.out_up == False:
                 res.pos_l = res.pos_l - 1
+                res.out_down = False
+            if res.pos_l == 0:
+                res.out_up = True
         elif button == "down":
-            if (res.pos_l >= 0 and res.pos_l < line_file - 1):
+            if res.out_down == False:
                 res.pos_l = res.pos_l + 1
-        #create_file(res.pos_l, res.pos_c, res.out_of_range)
+                res.out_up = False
+            if res.pos_l == line_file - 1:
+                res.out_down = True
+        for i in res.bals_pos:
+            if i[0] == res.pos_c and i[1] == res.pos_l:
+                res.bals_nbr = res.bals_nbr + 1
+                print(res.bals_nbr)
+        for j in res.movie_pos:
+            if j[0] == res.pos_c and j[1] == res.pos_l:
+                print(self.get_random_movie())
         self.save(res)
         context = {
             'col': col,
@@ -85,21 +97,25 @@ class Big_one():
         line = range(settings.GRID_SIZE[1])
         col_file = settings.GRID_SIZE[0]
         line_file = settings.GRID_SIZE[1]
-
         start = settings.START_POINT
         start_c = start[1]
         start_l = start[0]
-
         pos_c = start_c
         pos_l = start_l
+        out_right = False
+        out_left = False
+        out_up = False
+        out_down = False
+        bals_pos = self.create_moviebals_pos()
+        bals_nbr = 0
+        movie_pos = self.create_movie_pos(bals_pos)
         context = {
             'col': col,
             'line': line,
             'pos_l': pos_l,
             'pos_c': pos_c
         }
-        out_of_range = False
-        self.create_file(pos_l, pos_c, out_of_range)
+        self.create_file(bals_nbr, pos_l, pos_c, out_right, out_left, out_up, out_down, bals_pos, movie_pos)
         return render(request, 'base.html', context)
 
     class Movemone():
@@ -129,7 +145,32 @@ class Big_one():
         all_movies = dict()
         for i in settings.MOVIE_LIST:
             all_movies[i] = self.get_movie(i)
-        return all_movies 
+        return all_movies
+    
+    def create_moviebals_pos(self):
+        tab = []
+        col_nbr = settings.GRID_SIZE[0]
+        line_nbr = settings.GRID_SIZE[1]
+        nbr = (col_nbr * line_nbr) / 3
+        while (len(tab) != nbr):
+            i = [random.randint(0, col_nbr), random.randint(0, line_nbr)]
+            if i not in tab:
+                tab.append(i)
+        tab.sort()
+        return (tab)
+
+    def create_movie_pos(self, bals_pos):
+        movies = settings.MOVIE_LIST
+        pos = []
+        col_nbr = settings.GRID_SIZE[0]
+        line_nbr = settings.GRID_SIZE[1]
+        nbr = (col_nbr * line_nbr) / 3
+        while (len(pos) != nbr):
+            j = [random.randint(0, col_nbr), random.randint(0, line_nbr)]
+            if j not in pos and j not in bals_pos:
+                pos.append(j)
+        pos.sort()
+        return pos
     
 # def test(arg):
 #     a = Big_one()
