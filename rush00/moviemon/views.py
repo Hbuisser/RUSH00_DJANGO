35892d4a0a9dbc4ca0a3c4ce984e5a79eef2876b
Henry Buisseret <henrybuisseret@gmail.com>
2021-06-20 11:07:11 +0200

    ok

--- rush00/moviemon/views.py
+++ rush00/moviemon/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+def create_file(col, line, start_c, start_l)
+    with open(settings.LOG_FILE_PATH, 'a+') as f:
+
 def base(request):
-    return render(request, 'base.html')
+    col = range(settings.GRID_SIZE[0])
+    line = range(settings.GRID_SIZE[1])
+    start = settings.START_POINT
+    start_c = start[1]
+    start_l = start[0]
+    create_file(col, line, start_c, start_l)
+    context = {
+        'col': col,
+        'line': line,
+        'start_l': start_l,
+        'start_c': start_c
+    }
+    return render(request, 'base.html', context)
